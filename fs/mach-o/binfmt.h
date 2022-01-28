f61642e6cdc45b45ac303245f29bf4e50f1da341
Diego Magdaleno <diegomagdaleno@protonmail.com>
2022-01-28 13:59:21 -0600

    [Mach-O] Welcome Stock Mach-O darling loader

---
+++ fs/mach-o/binfmt.h
@@ -0,0 +1,12 @@
+#ifndef _BINFMT_H
+#define _BINFMT_H
+
+#include <duct/compiler/clang/asm-inline.h>
+#include <linux/binfmts.h>
+
+extern struct linux_binfmt macho_format;
+
+void macho_binfmt_init(void);
+void macho_binfmt_exit(void);
+
+#endif
