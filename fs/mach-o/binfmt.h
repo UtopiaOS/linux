da0c4c833e3627f6a4c8f3700df580437b434cd0
Diego Magdaleno <diegomagdaleno@protonmail.com>
2022-06-18 16:58:03 -0500

    [Remove] Let's start from scratch

--- fs/mach-o/binfmt.h
+++
@@ -1,9 +0,0 @@
-#ifndef _BINFMT_H
-#define _BINFMT_H
-
-//#include <duct/compiler/clang/asm-inline.h>
-#include <linux/binfmts.h>
-
-extern struct linux_binfmt macho_format;
-
-#endif
--- fs/mach-o/debug.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MACHO_DEBUG_H
-#define _MACHO_DEBUG_H
-
-#include <linux/kernel.h>
-
-#define mch_print_debug(x, args...) \
-    printk(KERN_DEBUG "binfmt_mach-o: " x, ##args)
-
-#endif
